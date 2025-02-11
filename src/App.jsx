14232919370143cb6e9ffa86983b32342c71b35e
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-02-10 21:02:39 -1200

    First Commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import './App.css'
+import Sidebar from '../components/Sidebar/Sidebar'
+
+function App() {
+  
+
+  return (
+    <>
+      <div className="App">
+        <Sidebar></Sidebar>
+        <div className="body">
+          
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default App
