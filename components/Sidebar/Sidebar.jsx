1231a13643a0a9e9284035bffaae8e757fb0172e
Oghenefega Omajene <omajeneoghenefega11@gmail.com>
2025-02-10 22:06:57 -1200

    SideNav Prototype Design

--- components/Sidebar/Sidebar.jsx
+++ components/Sidebar/Sidebar.jsx
@@ -1,9 +1,28 @@
 import React from 'react'
 import './Sidebar.css'
+import { TbLayoutDashboardFilled } from "react-icons/tb";
+import { FaCalendarAlt } from "react-icons/fa";
+import { IoMdSettings } from "react-icons/io";
+import { FaChartPie } from "react-icons/fa";
+import { FaSackDollar } from "react-icons/fa6";
+import { GiNotebook } from "react-icons/gi";
+import { IoLogOut } from "react-icons/io5";
+import logo from '../../images/logo2.png'
 
 const Sidebar = () => {
   return (
     <div className='sideNav'>
+      <img src={logo} alt="" />
+
+      <div className="sideNavLinks">
+        <a href=""><TbLayoutDashboardFilled /> Dashboard</a>
+        <a href=""><FaCalendarAlt /> Calendar</a>
+        <a href=""><FaChartPie /> Statistics</a>
+        <a href=""><FaSackDollar /> Loans</a>
+        <a href=""><GiNotebook /> Activities</a>
+      </div>
+        
+      <a href="" className='logOut'><IoLogOut /> Log Out</a>
         
     </div>
   )
